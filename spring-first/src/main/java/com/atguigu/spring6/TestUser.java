ebbbec1857315d3f61b0f5b7bb1550d8ce698582
sunxuecong <1668840900@qq.com>
2025-08-25 23:25:56 +0800

    feat: 测试类编写完成

--- spring-first/src/main/java/com/atguigu/spring6/TestUser.java
+++ spring-first/src/main/java/com/atguigu/spring6/TestUser.java
@@ -1,4 +1,22 @@
 package com.atguigu.spring6;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestUser {
+    @Test
+    public  void  testUserObject () {
+        // 加载spring配置文件，对象创建
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+
+        // 获取创建的对象
+        User user = (User)context.getBean("user");
+        System.out.println("1"+ user);
+        System.out.println("2");
+        // 使用对象调用方法进行测试
+        user.add();
+
+    }
+
 }
