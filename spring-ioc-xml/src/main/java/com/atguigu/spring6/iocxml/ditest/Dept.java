e0d3282f1e63e99ea59c912f9f9dfd6909d089cf
sunxuecong <1668840900@qq.com>
2025-09-01 22:23:25 +0800

    feat: List集合的注入

--- spring-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/ditest/Dept.java
+++ spring-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/ditest/Dept.java
@@ -1,11 +1,27 @@
 package com.atguigu.spring6.iocxml.ditest;
 
+import java.util.List;
+
 //部门类
 public class Dept {
     private String dname;
 
+    private List<Emp> empList;
+
+
+    public List<Emp> getEmpList() {
+        return empList;
+    }
+
+    public void setEmpList(List<Emp> empList) {
+        this.empList = empList;
+    }
+
     public  void info () {
         System.out.println("部门名称"+dname);
+        for(Emp emp:empList){
+            System.out.println(emp.getEname());
+        }
     }
 
     public String getDname() {
